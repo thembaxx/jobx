daf55ef73f146204bce7c6f1824818f6f7364440
Themba <mndebele.themba@gmail.com>
2025-09-23 22:04:21 +0200

    Implement code changes to enhance functionality and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,40 @@ export default function Home() {
 					<SearchBar />
 				</div>
 				<div className="px-6">
+					<div className="relative bg-white/80 border border-[#EBEDED] backdrop-blur-[25px] rounded-[20px] shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]">
+						<div className="absolute bottom-0 -right-8">
+							<Image
+								src="/Adobe Express - file (3).png"
+								alt=""
+								height={164}
+								width={164}
+							/>
+						</div>
+						<div className="p-4 space-y-4">
+							<h1 className="font-black text-[24px] leading-7 text-[#3C3C3C]">
+								Welcome to JobX
+							</h1>
+							<div className="flex jusstify-start">
+								<div className="text-white text-xs font-bold uppercase tracking-[3%] h-6 px-2.5 leading-0 bg-[#FC2215] rounded-tl-2xl rounded-br-2xl flex items-center">
+									Specials
+								</div>
+							</div>
+							<div className="flex">
+								<p className="max-w-[224px] text-sm">
+									Use code <b className="font-semibold">BETAAPP</b> and get{" "}
+									<span className="font-semibold">30% off</span> any orders from
+									our partners
+								</p>
+							</div>
+							<p className="text-xs text-[#6a6a6a">
+								Valid for the first{" "}
+								<span className="font-semibold text-[#3c3c3c]">1,000</span>{" "}
+								customers
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="px-6">
 					<div className="p-3 pb-2 space-y-3 bg-white/60 border border-[#EBEDED] backdrop-blur-[25px] rounded-[22px] showdow-[0px_7px_29px_0px rgba(100,100,111,0.2)]">
 						<p className="text-[10px] ml-1.5 uppercase font-semibold tracking-wider">
 							Most popular categories
